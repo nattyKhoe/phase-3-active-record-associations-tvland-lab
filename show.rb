169e4ace51217b06a63b27f2c62071cb2e713ed2
Natasha Kusuma <104250558+nattyKhoe@users.noreply.github.com>
2023-01-06 14:48:49 +1100

    Add files via upload
    done

---
+++ show.rb
@@ -0,0 +1,11 @@
+class Show < ActiveRecord::Base
+  has_many :characters
+  belongs_to :network
+
+  def actors_list
+    self.characters.map do |character|
+        character.actor.full_name
+    end
+  end
+  
+end
